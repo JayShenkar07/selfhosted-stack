1face9037c645c0821011798402bd5aac79ef1af
JayShenkar07 <jayshenkar27apr@gmail.com>
2025-04-14 22:26:42 +0530

    added bookstack

---
+++ bookstack/install.sh
@@ -0,0 +1,154 @@
+#!/bin/sh
+# This script will install a new BookStack instance on a fresh Ubuntu 20.04 server with SSL.
+# This script is experimental and does not ensure any security.
+
+# Fetch domain to use from first provided parameter,
+# Otherwise request the user to input their domain
+DOMAIN=$1
+if [ -z "$1" ]
+then
+  echo ""
+  printf "Enter the domain you want to host BookStack and press [ENTER]\nExamples: my-site.com or docs.my-site.com\n"
+  read -r DOMAIN
+fi
+
+# Ensure a domain was provided otherwise display
+# an error message and stop the script
+if [ -z "$DOMAIN" ]
+then
+  >&2 echo 'ERROR: A domain must be provided to run this script'
+  exit 1
+fi
+
+# Get the current machine IP address
+CURRENT_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+# Install core system packages
+apt update
+apt install -y software-properties-common
+export DEBIAN_FRONTEND=noninteractive
+add-apt-repository universe
+add-apt-repository -yu ppa:ondrej/php
+apt install -y git unzip apache2 curl certbot python3-certbot-apache php8.2 php8.2-curl \
+  php8.2-mbstring php8.2-ldap php8.2-xml php8.2-zip php8.2-gd php8.2-mysql mysql-client-8.0 \
+  libapache2-mod-php8.2
+
+# External SQL DB settings
+EXTERNAL_DB_HOST="<host_ip>"
+EXTERNAL_DB_PORT="3306"  # or custom port
+EXTERNAL_DB_DATABASE="<db>"
+EXTERNAL_DB_USERNAME="<username>"
+EXTERNAL_DB_PASSWORD="<db_password>"
+
+# Download BookStack
+cd /var/www || exit
+git clone https://github.com/BookStackApp/BookStack.git --branch release --single-branch bookstack
+BOOKSTACK_DIR="/var/www/bookstack"
+cd $BOOKSTACK_DIR || exit
+
+# Install composer
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid composer installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
+
+# Move composer to global installation
+mv composer.phar /usr/local/bin/composer
+
+# Install BookStack composer dependencies
+export COMPOSER_ALLOW_SUPERUSER=1
+php /usr/local/bin/composer install --no-dev --no-plugins
+
+# Copy and update BookStack environment variables
+cp .env.example .env
+sed -i.bak "s@APP_URL=.*\$@APP_URL=https://$DOMAIN@" .env
+sed -i.bak "s/DB_HOST=.*$/DB_HOST=$EXTERNAL_DB_HOST/" .env
+sed -i.bak "s/DB_PORT=.*$/DB_PORT=$EXTERNAL_DB_PORT/" .env
+sed -i.bak "s/DB_DATABASE=.*$/DB_DATABASE=$EXTERNAL_DB_DATABASE/" .env
+sed -i.bak "s/DB_USERNAME=.*$/DB_USERNAME=$EXTERNAL_DB_USERNAME/" .env
+sed -i.bak "s/DB_PASSWORD=.*\$/DB_PASSWORD=$EXTERNAL_DB_PASSWORD/" .env
+
+php artisan cache:clear
+# Generate the application key
+php artisan key:generate --no-interaction --force
+# Migrate the databases
+php artisan migrate --no-interaction --force
+
+# Set file and folder permissions
+chown www-data:www-data -R bootstrap/cache public/uploads storage && chmod -R 755 bootstrap/cache public/uploads storage
+
+# Set up apache
+a2enmod rewrite
+a2enmod php8.2
+a2enmod ssl
+
+cat >/etc/apache2/sites-available/bookstack.conf <<EOL
+<VirtualHost *:80>
+        ServerName ${DOMAIN}
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/bookstack/public/
+
+    <Directory /var/www/bookstack/public/>
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
+        <IfModule mod_rewrite.c>
+            <IfModule mod_negotiation.c>
+                Options -MultiViews -Indexes
+            </IfModule>
+
+            RewriteEngine On
+
+            # Handle Authorization Header
+            RewriteCond %{HTTP:Authorization} .
+            RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+
+            # Redirect Trailing Slashes If Not A Folder...
+            RewriteCond %{REQUEST_FILENAME} !-d
+            RewriteCond %{REQUEST_URI} (.+)/$
+            RewriteRule ^ %1 [L,R=301]
+
+            # Handle Front Controller...
+            RewriteCond %{REQUEST_FILENAME} !-d
+            RewriteCond %{REQUEST_FILENAME} !-f
+            RewriteRule ^ index.php [L]
+        </IfModule>
+    </Directory>
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        # Redirect HTTP to HTTPS
+        RewriteEngine On
+        RewriteCond %{HTTPS} off
+        RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+</VirtualHost>
+EOL
+
+a2dissite 000-default.conf
+a2ensite bookstack.conf
+
+# Restart apache to load new config
+systemctl restart apache2
+
+# Obtain and install SSL certificate with Certbot
+certbot --apache -d $DOMAIN --non-interactive --agree-tos --email jay.shenkar@bynry.io
+
+# Set up automatic SSL certificate renewal
+echo "0 0 * * * certbot renew --quiet" | crontab -
+
+echo ""
+echo "Setup Finished, Your BookStack instance should now be installed with HTTPS."
+echo "You can login with the email 'admin@admin.com' and password of 'password'."
+echo ""
+echo "You can access your BookStack instance at: https://$DOMAIN/"
